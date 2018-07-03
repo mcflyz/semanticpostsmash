98fbfa001206d162199e11f472af7e403527ca42
mcflyza <sorrentino.1807485@studenti.uniroma1.it>
2018-07-03 03:19:12 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # semanticpostsmash
-postsmash is a way to user's post management. 
+postsmash is a way to user's post management. <br><br>
+[Exercise for Academy, to using jQuery, Sass, Gulp. Thanks Prof. Del Medico] <br>
+using data by <a href="https://jsonplaceholder.typicode.com/">JSONPlaceholder</a>.
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp'),
+concat = require('gulp-concat'),
+cleanCSS = require('gulp-clean-css'),
+uglify = require('gulp-uglify-es').default;
+
+//all files are moved to dist
+//html -> update user html
+//css -> update user css
+//js -> update user js
+//js-lib -> update js libs
+//css-lib -> update css libs
+//default -> update user html, css, js
+//full -> update everything
+
+
+//alias gulp="./node_modules/gulp/bin/gulp.js"
+
+gulp.task('htm', () => gulp.src('app/*.htm*')
+                            .pipe(concat('index.htm'))
+                            .pipe(gulp.dest('dist/'))
+);
+
+gulp.task('css', () => gulp.src('app/css/*.css')
+                            .pipe(concat('style.css'))
+                            .pipe(cleanCSS())
+                            .pipe(gulp.dest('dist/css'))
+);
+
+gulp.task('js', () => gulp.src('app/js/*.js')
+                            .pipe(concat('script.js'))
+                            .pipe(uglify())
+                            .pipe(gulp.dest('dist/js'))
+);
+
+gulp.task('js-lib', () => gulp.src('node_modules/jquery/dist/jquery.min.js')
+                                .pipe(concat('lib.js'))
+                                .pipe(uglify())
+                                .pipe(gulp.dest('dist/js/lib'))
+);
+
+gulp.task('dev', gulp.series('htm', 'css', 'js'));
+gulp.task('prod', gulp.series('htm', 'css', 'js', 'js-lib'));
