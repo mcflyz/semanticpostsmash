3867734f368cad8608a5ca1664da63d774e86a4a
mcflyza <sorrentino.1807485@studenti.uniroma1.it>
2018-07-10 21:42:05 +0200

    js and css tabs completed

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,33 @@
-const newBtnWithClass = (btnClass) => $('<button></button>').attr('class', btnClass);
+const newBtnWithClass = (btnClass) => $('<button></button>').addClass(btnClass);
 const newBtnWithClassAndUserData = (btnClass, dataUserId) => newBtnWithClass(btnClass).attr('data-user-id', dataUserId);
+
 const newLabelWithFor = (forLabel) => $('<label></label>').attr('for', forLabel);
-const newSectionWithTitle = (title) => $('<section></section>').append('<h1></h1>').text(title);
-const newSectionWithTitleAndUserData = (title, dataUserId) => newSectionWithTitle(title).attr('data-user-id', dataUserId);
+
+const newSectionWithTitle = (title, dataUserId) => $('<section></section>').append($('<h1></h1>').attr('data-user-id', dataUserId).text(title));
+const newSectionWithTitleAndUserData = (title, dataUserId) => newSectionWithTitle(title, dataUserId).attr('data-buttons-id', dataUserId);
+
 const newParaWithText = (text) => $('<p></p>').text(text);
+const newParaWithTextAndClass = (text, paraClass) => newParaWithText(text).addClass(paraClass);
+
 const newDivWithClass = (divClass) => $('<div></div>').attr('class', divClass);
 const newDivWithClassAndId = (divClass, divId) =>  newDivWithClass(divClass).attr('id', divId);
+
 //hendler error
 const handleError = (xhr, status, error) => console.log(`ERROR!!\nStatus: ${status}\nMessage error:${error}`);
 
-//for posts from specific id user => https://jsonplaceholder.typicode.com/posts?userId=${id};
-//for todos from specific id user => https://jsonplaceholder.typicode.com/todos?userId=${id};
-//for albums from specific id user => https://jsonplaceholder.typicode.com/albums?userId=${id};
+function userData(ev){
+
+    let id = $(ev.target).attr('data-user-id');
+
+    findPostsOfSpecificUser(id);
+    findAlbumsOfSpecificUser(id);
+    findTodosOfSpecificUser(id);
+
+}
+
+const addPost = post => newSectionWithTitle(post.title).append($('<p></p>').text(post.body));
+const addAlbum = post => newSectionWithTitle(post.title).append($('<p></p>').text(post.body));
+const addTodo = todo => (todo.completed) ? newParaWithText(todo.title).css('color', 'greenyellow') : newParaWithText(todo.title).css('color', 'lightcoral');
 
 //generic find
 function find(url, callback) {
@@ -60,7 +76,7 @@ function createCard(user, whereAppend){
     divCard.append(section);
     $(whereAppend).append(divCard);
 
-    insertBtns(user.id,`*[data-user-id="${user.id}"]`);
+    insertBtns(user.id,`*[data-buttons-id="${user.id}"]`);
 
 }
 
@@ -69,21 +85,77 @@ function findPostsOfSpecificUser(id) {
     
     let url = `https://jsonplaceholder.typicode.com/posts?userId=${id}`;
 
-    callback = (posts) => {
-        console.log(posts);
-        posts.forEach( (post) => {
-            console.log(post);
-            //insertPosts();
-        });
-    }
-
+    callback = posts => createPosts(posts, '#postsHere');
+    
     find(url,callback);
+
+}
+
+//fetch albums from specific user id
+function findAlbumsOfSpecificUser(id){
+
+    let url = `https://jsonplaceholder.typicode.com/albums?userId=${id}`;
+
+    callback = albums => createAlbums(albums, '#albumsHere');
+    
+    find(url,callback);
+
+}
+
+//fetch todos from specific user id
+function findTodosOfSpecificUser(id){
+    
+    let url = `https://jsonplaceholder.typicode.com/todos?userId=${id}`;
+
+    callback = todos => createTodos(todos, '#todosHere');
+    
+    find(url,callback);
+
+}
+
+
+
+function createPosts(posts, whereAppend){
+
+    $('#postInit').css('display', 'none');
+
+    let divPosts = newDivWithClass('divPosts');
+
+    posts.forEach( (post) => $(divPosts).append(addPost(post).attr('class', 'post')));
+
+    $(whereAppend).html(divPosts);
+
+}
+
+function createAlbums(albums, whereAppend){
+
+    $('#postInit').css('display', 'none');
+
+    let divAlbum = newDivWithClass('divAlbums');
+
+    albums.forEach( (album) => $(divAlbum).append(addAlbum(album).attr('class', 'album')));
+
+    $(whereAppend).html(divAlbum);
+
+}
+
+function createTodos(todos, whereAppend){
+
+    $('#postInit').css('display', 'none');
+
+    let divTodo = newDivWithClass('divTodos');
+
+    todos.forEach( (todo) => {
+        $(divTodo).append(addTodo(todo).attr('class', 'todo'))
+        console.log(todo.completed);
+    });
+    
+
+    $(whereAppend).html(divTodo);
 
 }
 
 findUsers();
 
-//for(let i = 0; i<10; ++i) findPostsOfSpecificUser(i);
-const notify = ev => findPostsOfSpecificUser($(ev.target).parent('button').attr('data-user-id'));
-
-$('body').on( "click", '.btns button', notify);
+//eventListner on <h1> in '.card'
+$('body').on( "click", '.card h1', userData);
