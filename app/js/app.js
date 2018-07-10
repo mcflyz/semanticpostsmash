692c94d677e59c30aa048873861636fba37c4864
Student01 <antonio.mcfly@gmail.com>
2018-07-10 15:20:11 +0200

    fix js

--- app/js/app.js
+++ app/js/app.js
@@ -1,19 +1,28 @@
 const newBtnWithId = (id) => $('<button></button>').attr('id', id);
 const newLabelWithFor = (forLabel) => $('<label></label>').attr('for', forLabel);
-const sectionWithTitle = (title) => $('<section></section>').append('<h1></h1>').text(title);
-const paraWithText = (text) => $('<p></p>').text(text);
+const newSectionWithTitle = (title) => $('<section></section>').append('<h1></h1>').text(title);
+const newSectionWithTitleAndDataUser = (title, id) => newSectionWithTitle(title).attr('data-user-id', id);
+const newParaWithText = (text) => $('<p></p>').text(text);
 const newDivWithClass = (divClass) => $('<div></div>').attr('class', divClass);
 const newDivWithClassAndId = (divClass, divId) =>  newDivWithClass(divClass).attr('id', divId);
+//hendler error
+const handleError = (xhr, status, error) => console.log(`ERROR!!\nStatus: ${status}\nMessage error:${error}`);
+
+//for posts from specific id user => https://jsonplaceholder.typicode.com/posts?userId=${id};
+//for todos from specific id user => https://jsonplaceholder.typicode.com/todos?userId=${id};
+//for albums from specific id user => https://jsonplaceholder.typicode.com/albums?userId=${id};
 
 //generic find
 function find(url, callback) {
+    
     $.ajax({
         type: "GET",
         url: url,
         dataType: "json",
         success: (data) => callback(data),
-        failed: console.log('ERROR')
+        error: handleError
     });
+
 }
 
 //fetch users
@@ -21,14 +30,10 @@ function findUsers() {
     
     let url = 'https://jsonplaceholder.typicode.com/users';
 
-    callback = (data) => {
-        console.log(data);
-        data.forEach( (user) => {
-            createCard(user, '.usersContent');
-            console.log(data);
-        });
-    }
-
+    callback = users => 
+                    users.forEach( user => 
+                         createCard(user, '.usersContent'));
+    
     find(url,callback);
 
 }
@@ -45,17 +50,39 @@ function insertBtns(whereAppend){
 
 function createCard(user, whereAppend){
     
-    let divCard = newDivWithClassAndId('card', `card${user.id}`);
-    let section = sectionWithTitle(user.name);
+    let divCard = newDivWithClass('card');
+    let section = newSectionWithTitleAndDataUser(user.name, user.id);
     let imgCard = $('<img></img>').attr('src', `img/card${user.id}.png`);
-    let parCard = paraWithText(user.email);
+    let parCard = newParaWithText(user.email);
 
     section.append(imgCard, parCard);
     divCard.append(section);
     $(whereAppend).append(divCard);
 
-    insertBtns(`#card${user.id}`);
+    insertBtns(`*[data-user-id="${user.id}"]`);
+
+}
+
+//fetch posts from specific user id
+function findPostsOfSpecificUser(id) {
+    
+    let url = `https://jsonplaceholder.typicode.com/posts?userId=${id}`;
+
+    callback = (posts) => {
+        console.log(posts);
+        posts.forEach( (post) => {
+            console.log(post);
+            //insertPosts();
+        });
+    }
+
+    find(url,callback);
 
 }
 
 findUsers();
+
+//for(let i = 0; i<10; ++i) findPostsOfSpecificUser(i);
+const notify = (event) => console.log(event.target);
+
+$('.btns button').on( "click", notify );
