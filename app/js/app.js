657622cd278155be47de99858d92dbf88f21cb29
Student01 <antonio.mcfly@gmail.com>
2018-07-11 10:18:08 +0200

    bugs fix

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,7 @@ const newParaWithText = (text) => $('<p></p>').text(text);
 const newParaWithTextAndClass = (text, paraClass) => newParaWithText(text).addClass(paraClass);
 
 const newDivWithClass = (divClass) => $('<div></div>').attr('class', divClass);
-const newDivWithClassAndId = (divClass, divId) =>  newDivWithClass(divClass).attr('id', divId);
+const newDivWithClassAndId = (divClass, divId) => newDivWithClass(divClass).attr('id', divId);
 
 const newLink = (href) => $('<a></a>').attr('href', href);
 
@@ -26,7 +26,7 @@ const getId = (event) => $(event.target).attr('data-user-id');
 //hendler error
 const handleError = (xhr, status, error) => console.log(`ERROR!!\nStatus: ${status}\nMessage error:${error}`);
 
-function userData(ev){
+function userData(ev) {
 
     let id = getId(ev);
 
@@ -38,7 +38,7 @@ function userData(ev){
 
 //generic find
 function find(url, callback) {
-    
+
     $.ajax({
         type: "GET",
         url: url,
@@ -57,37 +57,50 @@ function remove(url) {
         success: function (data, textStatus, jqXHR) {
             console.log("remove successfully");
         },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log("remove error: " + textStatus);
-        }
+        error: handleError
     });
+}
+
+//modify user 
+function update(url, input) { 
+    $.ajax({ 
+        type: "PUT", 
+        contentType: "application/json", 
+        url: url, 
+        dataType: "json", 
+        data: input, 
+        success: function (data, textStatus, jqXHR) { 
+            console.log("update successfully"); 
+        }, 
+        error: handleError
+    }); 
 }
 
 //fetch users
 function findUsers() {
-    
+
     let url = 'https://jsonplaceholder.typicode.com/users';
 
-    callback = users => 
-                    users.forEach( user => 
-                         createCard(user, '.usersContent'));
-    
-    find(url,callback);
+    callback = users =>
+        users.forEach(user =>
+            createCard(user, '.usersContent'));
+
+    find(url, callback);
 
 }
 
 //fetch user
 function findUserById(id, callback) {
-    
+
     let url = `https://jsonplaceholder.typicode.com/users/${id}`;
 
     callback = user => showInfo(user, '#modalView');
-    
-    find(url,callback);
+
+    find(url, callback);
 
 }
 
-function insertBtns(id, whereAppend){
+function insertBtns(id, whereAppend) {
 
     let btnView = newBtnWithClassAndUserData('view', id).append(newLabelWithFor('view'));
     let btnModify = newBtnWithClassAndUserData('modify', id).append(newLabelWithFor('modify'));
@@ -105,8 +118,8 @@ function insertBtns(id, whereAppend){
 
 };
 
-function createCard(user, whereAppend){
-    
+function createCard(user, whereAppend) {
+
     let divCard = newDivWithClass('card');
     let section = newSectionWithTitleAndUserData(user.name, user.id);
     let imgCard = $('<img></img>').attr('src', `img/card${user.id}.png`);
@@ -116,81 +129,81 @@ function createCard(user, whereAppend){
     divCard.append(section);
     $(whereAppend).append(divCard);
 
-    insertBtns(user.id,`*[data-buttons-id="${user.id}"]`);
+    insertBtns(user.id, `*[data-buttons-id="${user.id}"]`);
 
 }
 
 //fetch posts from specific user id
 function findPostsOfSpecificUser(id) {
-    
+
     let url = `https://jsonplaceholder.typicode.com/posts?userId=${id}`;
 
     callback = posts => createPosts(posts, '#postsHere');
-    
-    find(url,callback);
+
+    find(url, callback);
 
 }
 
 //fetch albums from specific user id
-function findAlbumsOfSpecificUser(id){
+function findAlbumsOfSpecificUser(id) {
 
     let url = `https://jsonplaceholder.typicode.com/albums?userId=${id}`;
 
     callback = albums => createAlbums(albums, '#albumsHere');
-    
-    find(url,callback);
+
+    find(url, callback);
 
 }
 
 //fetch todos from specific user id
-function findTodosOfSpecificUser(id){
-    
+function findTodosOfSpecificUser(id) {
+
     let url = `https://jsonplaceholder.typicode.com/todos?userId=${id}`;
 
     callback = todos => createTodos(todos, '#todosHere');
-    
-    find(url,callback);
+
+    find(url, callback);
 
 }
 
-function createPosts(posts, whereAppend){
+function createPosts(posts, whereAppend) {
 
     $('#postInit').css('display', 'none');
 
     let divPosts = newDivWithClass('divPosts');
 
-    posts.forEach( (post) => $(divPosts).append(addPost(post).attr('class', 'post')));
+    posts.forEach((post) => $(divPosts).append(addPost(post).attr('class', 'post')));
 
     $(whereAppend).html(divPosts);
 
 }
 
-function createAlbums(albums, whereAppend){
+function createAlbums(albums, whereAppend) {
 
-    $('#postInit').css('display', 'none');
+    $('#albumInit').css('display', 'none');
 
     let divAlbum = newDivWithClass('divAlbums');
 
-    albums.forEach( (album) => $(divAlbum).append(addAlbum(album).attr('class', 'album')));
+    albums.forEach((album) => $(divAlbum).append(addAlbum(album).attr('class', 'album')));
 
     $(whereAppend).html(divAlbum);
 
 }
 
-function createTodos(todos, whereAppend){
+function createTodos(todos, whereAppend) {
 
-    $('#postInit').css('display', 'none');
+    $('#todoInit').css('display', 'none');
 
     let divTodo = newDivWithClass('divTodos');
 
-    todos.forEach( (todo) => $(divTodo).append(addTodo(todo).attr('class', 'todo')));
-    
+    todos.forEach((todo) => $(divTodo).append(addTodo(todo).attr('class', 'todo')));
+
 
     $(whereAppend).html(divTodo);
 
 }
 
-function showInfo(user, whereAppend){
+function showInfo(user, whereAppend) {
 
     let section = newSectionWithTitle(user.name);
     let address = "street: " + user.address.street + "suite: " + user.address.suite + " city: " + user.address.city;
@@ -211,13 +224,13 @@ function showInfo(user, whereAppend){
 findUsers();
 
 //eventListner on <h1> in '.card'
-$('body').on( "click", '.card h1', userData);
+$('body').on("click", '.card h1', userData);
 
 //eventListner on view button in '.btns'
-$('body').on( "click", '.btns .view', modalView);
+$('body').on("click", '.btns .view', modalView);
 
 //eventListner on modify button in '.btns'
-$('body').on( "click", '.btns .modify', modalModify);
+$('body').on("click", '.btns .modify', modalModify);
 
 //eventListner on delete button in '.btns'
-$('body').on( "click", '.btns .delete', modalDelete);
+$('body').on("click", '.btns .delete', modalDelete);
